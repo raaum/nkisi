451e15cc65c1bf15a9f9146e8f6b7d5ce23cba0f
Greg Raaum <greg.raaum@yahoo.com>
2016-01-21 22:47:31 -0600

    adding some style

--- server.js
+++ server.js
@@ -2,13 +2,10 @@ var express  = require('express');
 var login    = require('./login');
 
 /*
-
 Use the Express Server and EJS view engine
-
 - Serve requests for static files located in '/public'
 - For all other requests serve 'index'
 - Listen on port 3333
-
 */
 
 express()
@@ -16,8 +13,6 @@ express()
   .use(express.static('./public'))
   .use(login.routes)
   .get('*', login.required, function (req, res) {
-    res.render('index', {
-    	user: login.safe(req.user)
-    });
+    res.render('index', { user: login.safe(req.user) });
   })
   .listen(3333);
